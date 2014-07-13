c4bb0c6e5690c70ae341f656e62ebdda8491573c
eloy <eloy.colell@gmail.com>
2014-07-13 20:54:28 -0300

    It try to fix the compilation error when the pip installation is used.

--- setup.py
+++ setup.py
@@ -16,7 +16,15 @@ import progressbar as pb
 import tarfile as tar
 import multiprocessing
 
-update_shared_libs = ''
+os_name = p.system()
+title = '%s %s' % (os_name, p.architecture()[0])
+bar = '-' * len(title)
+print '+%s+' % bar
+print '|%s|' % title
+print '+%s+' % bar
+source_path = '/usr/sources'
+os.system('sudo mkdir -p %s' % source_path)
+os.system('sudo chmod -R ugo+rwx %s' % source_path)
 
 systems = {
     'Linux': {
@@ -35,13 +43,6 @@ systems = {
     }
 }
 
-os_name = p.system()
-title = '%s %s' % (os_name, p.architecture()[0])
-bar = '-' * len(title)
-print '+%s+' % bar
-print '|%s|' % title
-print '+%s+' % bar
-
 
 class FakeAgent(FancyURLopener, object):
     version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) '
@@ -52,9 +53,11 @@ class FakeAgent(FancyURLopener, object):
 downloader = FakeAgent()
 
 
-def get(path, url):
+def get(path, url, local_path):
     filename = '%s.tar.gz' % path
-    if not os.path.isfile(filename):
+    local_filename = '%s/%s' % (local_path, filename)
+    local_unpacked = '%s/%s' % (local_path, path)
+    if not os.path.isfile('%s/%s' % (local_path, filename)):
         widgets = [filename, '  ', pb.Percentage(), ' ',
                    pb.Bar('#', u'\033[34m[', u']\033[0m'),
                    ' ', pb.ETA(), ' ', pb.FileTransferSpeed()]
@@ -65,35 +68,41 @@ def get(path, url):
             status = (count * blockSize if totalSize >= count * blockSize
                       else totalSize)
             bar.update(status)
-        downloader.retrieve('%s/%s' % (url, filename), filename,
+        downloader.retrieve('%s/%s' % (url, filename), local_filename,
                             reporthook=dlProgress)
-    if not os.path.isdir(path):
-        tfile = tar.open(filename, mode='r:gz')
-        tfile.extractall('.')
+    if not os.path.isdir(local_unpacked):
+        tfile = tar.open(local_filename, mode='r:gz')
+        tfile.extractall(local_path)
         tfile.close()
 
 
 def build(path, pre_config='', post_config=''):
-    filename = systems[os_name]['libs'][path.split('-')[0]]
+    lib_key = path.split('-')[0].split('/')[-1]
+    filename = systems[os_name]['libs'][lib_key]
     if os.path.isfile(filename):
         os.system('sudo rm %s' % filename)
     ncores = multiprocessing.cpu_count()
     os.system('cd %s; %s ./configure %s; make -j %s;'
               ' sudo make install'  # check
               % (path, pre_config, post_config, ncores))
+    update_shared_libs = systems[os_name]['update_shared_libs']
     if update_shared_libs:
-        os.system(systems[os_name]['update_shared_libs'])
+        os.system(update_shared_libs)
 
 
 def install_libs():
+    global source_path
+    # Deploy the hdf5 C library
     name = 'hdf5-%s' % '1.8.12'
     get(name,
-        'http://www.hdfgroup.org/ftp/HDF5/releases/%s/src' % (name))
-    build(name,
+        'http://www.hdfgroup.org/ftp/HDF5/releases/%s/src' % (name),
+        source_path)
+    build('%s/%s' % (source_path, name),
           post_config='--prefix=/usr/local --enable-shared --enable-hl')
+    # Deploy the netcdf4 C library
     name = 'netcdf-%s' % '4.3.1-rc4'
-    get(name, 'ftp://ftp.unidata.ucar.edu/pub/netcdf')
-    build(name,
+    get(name, 'ftp://ftp.unidata.ucar.edu/pub/netcdf', source_path)
+    build('%s/%s' % (source_path, name),
           pre_config='LDFLAGS=-L/usr/local/lib CPPFLAGS=-I/usr/local/include '
           'LD_LIBRARY_PATH=/usr/local',
           post_config='--enable-netcdf-4 --enable-dap --enable-shared'
