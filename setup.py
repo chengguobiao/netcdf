c74677e9a82ff0a0f5d89e9d7b4f0bb705ec1392
eloy <eloy.colell@gmail.com>
2014-07-18 23:59:55 -0300

    Try to improve the auto version with git tags.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def calculate_version():
     version_py = os.path.join(os.path.dirname(__file__), 'version.py')
     try:
         version_git = subprocess.check_output(["git", "describe"]).rstrip()
-    except:
+    except Exception:
         with open(version_py, 'r') as fh:
             version_git = (open(version_py).read()
                            .strip().split('=')[-1].replace('"', ''))
